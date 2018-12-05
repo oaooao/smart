b6715299ce696e60a395f02a653186f4126f9313
oaooao <1379199882@qq.com>
2018-12-05 15:57:10 +0800

    a

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,25 @@ module.exports = {
   devServer: {
     proxy: {
       '/robot': {
-        target: 'http://192.168.8.100:9090/',
+        target: 'http://192.168.8.101:9090/',
         secure: false,
         changeOrigin: true,
         ws: true
         // pathRewrite: {
         //   '^/robot': ''
         // }
+      },
+      '/oauth': {
+        target: 'https://aip.baidubce.com/',
+        secure: false,
+        changeOrigin: true,
+        ws: true
+      },
+      '/server_api': {
+        target: 'https://vop.baidu.com/',
+        secure: false,
+        changeOrigin: true,
+        ws: true
       }
     }
   },
